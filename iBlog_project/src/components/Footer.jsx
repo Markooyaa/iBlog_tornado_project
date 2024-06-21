a39470934fd4f6f9c3e0473d0b42e0464eebbcca
home <duluumgl1112@gmail.com>
2024-06-22 03:19:07 +0800

    add:

--- iBlog_project/src/components/Footer.jsx
+++ iBlog_project/src/components/Footer.jsx
@@ -16,6 +16,41 @@ export default function Footer() {
                 </h2>
               </div>
               <p className="text-[10px] text-slate-600">Write, Read, Enjoy</p>
+=======
+        <div className="inline-flex sm:flex flex-col items-start gap-[20px] sm:gap-[10px] px-[65px] py-[25px] border-t-[1px] sm:h-[190px]">
+            <div className="content flex-col sm:flex-row flex gap-[20px] sm:gap-[15px] items-start">
+                <div className="flex flex-col items-start w-[216px]">
+                    <div className="h-[52px]">
+                        <div className="flex items-center">
+                            <img src="Images/Group.png" />
+                            <h2 className="text-orange-400 text-[28px] font-normal w-[63px] h-[42px]">iBlog</h2>
+                        </div>
+                        <p className="text-[10px] text-slate-600">Write, Read, Enjoy</p>
+                    </div>
+                </div>
+                <div className="flex sm:justify-center items-start gap-[60px]">
+                    <div className="flex flex-col items-start gap-[18px]">
+                        <h3 className="text-[16px] font-semibold leading-[21px]">Тусламж</h3>
+                        <div className="flex flex-col items-start gap-[6px]">
+                            <a href="" className="text-[14px] leading-[18px]">Хэрэглэх заавар</a>
+                            <a href="" className="text-[14px] leading-[18px]">Санал хүсэлт</a>
+                        </div>
+                    </div>
+                    <div className="category flex flex-col items-start gap-[18px]">
+                        <h3 className="text-[16px] font-semibold leading-[21px]">Бидэнтэй холбогдох</h3>
+                        <div className="contact flex flex-col items-start gap-[8px]">
+                            <div className="email flex items-center gap-[2px]">
+
+                                <a href="" className="text-[14px] leading-[18px]">info@jstemplate.net</a>
+                            </div>
+                            <div className="phone flex items-center gap-[2px]">
+
+                                <a href="" className="text-[14px] leading-[18px]">+976 99112344</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+>>>>>>> 8186f9b (add:)
             </div>
           </div>
           <div className="flex sm:justify-center items-start gap-[60px]">
