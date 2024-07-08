25fbb04f93e60e5397096241e03bd6e35e3f17c7
home <duluumgl1112@gmail.com>
2024-07-08 18:49:45 +0800

    add:

--- iBlog_project/src/pages/LoginPage.jsx
+++ iBlog_project/src/pages/LoginPage.jsx
@@ -24,11 +24,11 @@ export function LoginPage() {
 
   
     return (
-        <>
+        <div className="flex justify-between flex-col h-full">
 
             <Login_Create_header text=''></Login_Create_header>
 
-            <div id="computer" className="container flex-col flex justify-center items-center lg:pt-[130px] lg:pb-[220px] m-auto w-[100%] lg:h-[850px] ">
+            <div id="computer" className="container h-full flex-col flex justify-center items-center lg:pt-[130px] lg:pb-[220px] m-auto w-[100%] lg:h-[850px] ">
                 <div className="  lg:w-[478px] w-[335px] h-[50px]  hidden lg:flex "> <p className="text-[#E86B02] text-start pt-[10px] text-[20px]">Нэвтрэх</p></div>
                 <div className="logo lg:hidden flex flex-col">
                     <div className=" w-[100%] justify-center flex items-center  ">
@@ -88,6 +88,6 @@ export function LoginPage() {
 
 
             <Login_Create_footer />
-        </>
+        </div>
     )
 }
